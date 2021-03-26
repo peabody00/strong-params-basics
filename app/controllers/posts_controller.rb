7686682a93132e9690ce998bb3c5fabb8134a239
Charlie <peabody00@gmail.com>
2021-03-26 08:29:46 -0500

    commit tree d30dde9bceaaf79d56b954eb6309cbbfe0c83bfb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,45 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+		#   @post = Post.new(params["post"])
+		#   @post.save
+		#   redirect_to post_path(@post)
+		#   REFACTOR 1 @post = Post.new(params.require(:post).permit(:title, :description))
+		# REFACTOR 2 @post = Post.new(post_params)
+		@post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
+		#   @post = Post.find(params[:id])
+		#   @post.update(params["post"])
+		#   redirect_to post_path(@post)
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+		#   REFACTOR 1 @post.update(params.require(:post).permit(:title))
+		# REFACTOR 2 @post.update(post_params)
+		@post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+
+	# REFACTOR 2
+	# def post_params
+  	# 	params.require(:post).permit(:title, :description)
+	# end
+
+	# We pass the permitted fields in as *args;
+	# this keeps `post_params` pretty dry while
+	# still allowing slightly different behavior
+	# depending on the controller action. This
+	# should come after the other methods
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
+  	end 
 end
